cd2154f79cdccd92823f91193591fe3b0ab2e6ab
Diptesh Patel <diptesh.patel@rakuten.com>
2024-06-26 10:04:24 +1000

    Update bridging header

--- ios-autofill-cocoapods-integration/FillrPods/FillrPods-Bridging-Header.h
+++ ios-autofill-cocoapods-integration/FillrPods/FillrPods-Bridging-Header.h
@@ -5,20 +5,9 @@
 #import <FillrSDK/Fillr.h>
 #import <FillrSDK/FillrWidgetAuth.h>
 #import <FillrSDK/FillrConfig.h>
-//#import <FillrSDK/FillrCartInformationExtraction.h>
 
 #import <FillrAutofillSDK/FillrAutofill.h>
 #import <FillrAutofillSDK/FillrAutofillDelegate.h>
 #import <FillrAutofillSDK/HeadlessBaseFillProvider.h>
-
-////
-//
-
-//#import "Fillr.h"
-//#import "HeadlessBaseFillProvider.h"
-//#import "FillrWidgetAuth.h"
-//#import "FillrConfig.h"
-//#import "FillrCartInformationExtraction.h"
-
 
 
