ac65a53d6cf2d59b57a5ff191c370fd003a5955d
Diptesh Patel <diptesh.patel@rakuten.com>
2024-06-27 15:58:42 +1000

    Remove commented code

--- ios-fillr-cocoapods-integration/FillrPods/AppDelegate.swift
+++ ios-fillr-cocoapods-integration/FillrPods/AppDelegate.swift
@@ -9,7 +9,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-//    var autofill:FillrAutofill?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
